7be6ebb18e9374726a41527b551dc165437c7a7f
Orochyy <yura333619@gmail.com>
2022-01-25 12:46:41 +0200

    func

--- migrations/db-migrate.go
+++ migrations/db-migrate.go
@@ -1,1 +1,13 @@
 package migrations
+
+import (
+	"BitmaxGinGorilla/config"
+	"GinApiGormMysqlElif/entity"
+	"gorm.io/gorm"
+)
+
+var db *gorm.DB = config.SetupDatabaseConnection()
+
+func DbMigrate() {
+	db.AutoMigrate(&entity.User{})
+}
