322b47afd23a209319c2cb2dc610c42387627238
Orochyy <yura333619@gmail.com>
2022-01-25 19:22:51 +0200

    create subscribe-service.go && subscribe-dto.go +  Create + KillSubscribe

--- service/subscribe-service.go
+++ service/subscribe-service.go
@@ -1,10 +1,38 @@
 package service
 
+import (
+	"BitmaxGinGorilla/dto"
+	"BitmaxGinGorilla/entity"
+	"BitmaxGinGorilla/repository"
+	"github.com/mashingan/smapping"
+	"log"
+)
+
 type SubscribeService interface {
 	//Create
 	//Delete
 }
 
 type subscribeService struct {
-	subscribeRepository
+	subscribeRepository repository.SubscribeRepository
+}
+
+func NewSubService(subscribeRepo repository.SubscribeRepository) subscribeService {
+	return &subscribeService{
+		subscribeRepository: subscribeRepo,
+	}
+}
+
+func (service *subscribeService) Create(s dto.Subsription) entity.Subsribe {
+	subscribe := entity.Subsribe{}
+	err := smapping.FillStruct(&subscribe, smapping.MapFields(&s))
+	if err != nil {
+		log.Fatalf("Failed map %v: ", err)
+	}
+	res := service.subscribeRepository.CreateSubscribe(subscribe)
+	return res
+}
+
+func (service *subscribeService) Delete(s entity.Subsribe) {
+	service.subscribeRepository.KillSubscribe(s)
 }
